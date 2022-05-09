10e4a66d20e4f766d28bb8a4387cf3b3f00ae45a
Telesphoreo <me@telesphoreo.me>
2022-05-08 21:12:15 -0500

    Attempt to fix compilation errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     compileOnly("dev.plex:server:1.1-SNAPSHOT")
     compileOnly("dev.morphia.morphia:morphia-core:2.2.6")
     compileOnly("org.json:json:20220320")
+    implementation("org.jetbrains:annotations:23.0.0")
 }
 
 group = "dev.plex"
